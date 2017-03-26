bc002e31c7ad09ce7a8bf07bfecfe5bc87cfc63f
shashi <shashi@mbpwithtouchbar.lan>
2017-03-26 13:20:00 -0700

    deploy changes

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
 # copy files to shiny server dir
 git pull
 
+chmod +x deplpy.sh
+chmod 777 index.Rmd
+
 cp index.Rmd /srv/shiny-server/taxed/
 cp tax_engine.R /srv/shiny-server/taxed/
 cp eic.feather /srv/shiny-server/taxed/
 cp marriage-cost.feather /srv/shiny-server/taxed/
 cp google-analytics.js /srv/shiny-server/taxed/
+
+git stash
